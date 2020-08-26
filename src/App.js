504d79dcac03b3b86b86866da562d64a67b2d68c
Marcos Zaghetto <jr.zaghetto@gmail.com>
2020-08-25 21:14:30 -0300

    adjusts to pass in test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import api from './services/api';
 
 import {
   SafeAreaView,
-  View,
   FlatList,
   Text,
   StatusBar,
@@ -11,46 +12,77 @@ import {
 } from "react-native";
 
 export default function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleLikeRepository(id) {
     // Implement "Like Repository" functionality
+    await api.post(`repositories/${id}/like`)
+
+    const repositoriesUpdated = repositories.map(repository => {
+      if (repository.id === id) {
+        repository.likes++;
+        return repository
+      } else {
+        return repository
+      }
+    })
+    setRepositories(repositoriesUpdated)
+ 
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+          <FlatList 
+            data={repositories}
+            styles={styles.repository}
+            keyExtractor={repositories => repositories.id}
+            renderItem={({ item: repository }) => (
+              <SafeAreaView style={styles.repositoryContainer}>
+                <Text styles={styles.likeText} key={repository.id}>{repository.title}</Text>
+                <SafeAreaView style={styles.techsContainer}>
+                  {(typeof(repository.techs)) === 'string' 
+                  ?                   
+                  <Text style={styles.tech}>
+                    {repository.techs}
+                  </Text>
+                  : 
+                  repository.techs.map(techs => 
+                  <Text style={styles.tech}>
+                    {techs}
+                  </Text>)}
+                </SafeAreaView>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+                <SafeAreaView style={styles.likesContainer}>
+                  <Text
+                    style={styles.likeText}
+                    // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                    testID={`repository-likes-${repository.id}`}
+                  >
+                    {repository.likes} curtidas
+                  </Text>
+                </SafeAreaView>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleLikeRepository(repository.id)}
+                  // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                  testID={`like-button-${repository.id}`}
+                >
+                  <Text style={styles.buttonText}>Curtir</Text>
+                </TouchableOpacity>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
+              </SafeAreaView>
+            )}
+          />
+        </SafeAreaView>
     </>
   );
 }
